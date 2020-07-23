3c959805d3640f31a4691c32c908f64a94bce2d9
Grant Allard <grantallard.dev@gmail.com>
2020-07-23 08:11:02 -0400

    Updates to First Commit.

--- .history/pages/inspire_20200722224355.vue
+++ .history/pages/inspire_20200722224355.vue
@@ -8,7 +8,6 @@
       >
       <blockquote class="blockquote">
         &#8220;First, solve the problem. Then, write the code.&#8221;
-        
         <footer>
           <small>
             <em>&mdash;John Johnson</em>
